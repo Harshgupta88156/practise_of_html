99452ecf5c017ea77cd20937521213c76088eccf
Harsh Gupta <harshgupta88156@gmail.com>
2024-06-16 16:20:07 +0530

    practsing html js

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+/**
+ * basic variable naming
+ * dynamically typed language 
+*/
+
+console.log(10/3);
+
+let a ="op";
+let b = " role";
+console.log(a+b);
+let c= 6;
+if(c % 3 == 0){
+    console.log(c,"is a multiple of 3");
+}
+
+
